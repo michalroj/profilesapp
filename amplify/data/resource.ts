f018de4fcae3838e85205b965293d50ce6742e0f
Michal Roj <michal.roj@jtendo.com>
2025-03-08 22:58:05 +0100

    displaying user profile

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,11 +1,43 @@
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { postConfirmation } from "../auth/post-confirmation/resource";
+
+const schema = a
+  .schema({
+    UserProfile: a
+      .model({
+        email: a.string(),
+        profileOwner: a.string(),
+      })
+      .authorization((allow) => [
+        allow.ownerDefinedIn("profileOwner"),
+      ]),
+  })
+  .authorization((allow) => [allow.resource(postConfirmation)]);
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "apiKey",
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
+  },
+});
+
+
+/*
+
+
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any unauthenticated user can "create", "read", "update", 
-and "delete" any "Todo" records.
-=========================================================================*/
+//== STEP 1 ===============================================================
+//The section below creates a Todo database table with a "content" field. Try
+//adding a new "isDone" field as a boolean. The authorization rule below
+//specifies that any unauthenticated user can "create", "read", "update", 
+//and "delete" any "Todo" records.
+//=========================================================================
+
 const schema = a.schema({
   Todo: a
     .model({
@@ -22,6 +54,8 @@ export const data = defineData({
     defaultAuthorizationMode: 'iam',
   },
 });
+*/
+
 
 /*== STEP 2 ===============================================================
 Go to your frontend source code. From your client-side code, generate a
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Authenticator } from "@aws-amplify/ui-react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Authenticator>
+      <App />
+    </Authenticator>
+  </React.StrictMode>
+);
+
+/*
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -8,3 +23,4 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+*/
